8737fd12a1357add41531f1768f1a20381cc8e10
Ewa Adamska <ewa.adamska@fuw.edu.pl>
2019-10-25 13:17:00 +0200

    exceptions for bg files and xlsx gamma marks files added

--- spectrum_plotter.py
+++ spectrum_plotter.py
@@ -4,13 +4,6 @@ from matplotlib.widgets import Button
 import pandas as pd
 
 
-
-####
-# TODO: different colors of marks?
-####
-
-
-
 def read_data(filename, bg_filename=''):
     #prepare Data Frame
     spect = pd.read_csv(filename, skiprows=1, names=['spectrum'])
@@ -42,20 +35,28 @@ def addEnergy(energy, y, text, shiftx=0, fontsize=10):
 
 if __name__=='__main__':
 
-    GATES_LIST = (1436, 539)
+    GATES_LIST = (538, 444)
     X_RANGE = (300, 500)
-    Y_RANGE = (0, 100)
+    Y_RANGE = (0, 250)
     SAVE_OUTPUT = False
     PRELIMINARY = False
 
     gatename="g{}g{}".format(GATES_LIST[0], GATES_LIST[1])
-    data = read_data(filename="gate_{}.txt".format(gatename), bg_filename='bg_{}.txt'.format(gatename))
-    gamma_marks = pd.read_excel('gamma_marks_{}.xlsx'.format(gatename))
+    try:
+        data = read_data(filename="Gates/gate_{}.txt".format(gatename), bg_filename='Gates/bg_{}.txt'.format(gatename))
+    except FileNotFoundError:
+        data = read_data(filename="Gates/gate_{}.txt".format(gatename))
 
+    try:
+        gamma_marks = pd.read_excel('gamma_marks_{}.xlsx'.format(gatename))
+    except FileNotFoundError:
+        gamma_marks = False
     # fig = plt.figure(figsize=(10, 4))
     # ax = fig.add_subplot()
 
     fig, ax = plt.subplots()
+    fig.set_size_inches(10, 5)
+    fig.subplots_adjust(bottom=0.5)
     ax.tick_params(axis='both', labelsize=14)
     ax.set_xlabel('Energy (keV)', fontsize=12)
     ax.set_ylabel('Number of counts', fontsize=12)
@@ -70,16 +71,15 @@ if __name__=='__main__':
 
     ax.text(ax.get_xlim()[1]-1.2*box_width, ax.get_ylim()[1]-1.1*box_height, "gate {} - {} keV ".format(GATES_LIST[0], GATES_LIST[1]), fontsize=12)
 
-
-
     #adding marks:
-    for index, gamma in gamma_marks.iterrows():
-        if gamma.type == 'text':
-            addEnergy(energy=int(gamma.energy), text="{} ({})".format(gamma.energy, gamma.isotope), y=gamma.y, shiftx=gamma.shiftx, fontsize=12)
-        elif gamma.type == 'arrow':
-            addArrow(x=int(gamma.energy), text="{} ({})".format(gamma.energy, gamma.isotope), y=gamma.y, length=gamma.length, shiftx=gamma.shiftx)
-        elif gamma.type == 'sign':
-            addEnergy(energy=int(gamma.energy), text="{}".format(gamma.isotope), y=gamma.y, shiftx=gamma.shiftx, fontsize=20)
+    if gamma_marks:
+        for index, gamma in gamma_marks.iterrows():
+            if gamma.type == 'text':
+                addEnergy(energy=int(gamma.energy), text="{} ({})".format(gamma.energy, gamma.isotope), y=gamma.y, shiftx=gamma.shiftx, fontsize=12)
+            elif gamma.type == 'arrow':
+                addArrow(x=int(gamma.energy), text="{} ({})".format(gamma.energy, gamma.isotope), y=gamma.y, length=gamma.length, shiftx=gamma.shiftx)
+            elif gamma.type == 'sign':
+                addEnergy(energy=int(gamma.energy), text="{}".format(gamma.isotope), y=gamma.y, shiftx=gamma.shiftx, fontsize=20)
 
     # plotting the data
     plot, = plt.plot(data.spectrum, drawstyle='steps-mid', color='black', lw=1)
@@ -90,7 +90,7 @@ if __name__=='__main__':
 
     if PRELIMINARY:
         plt.text((ax.get_xlim()[1] + ax.get_xlim()[0])/2., (ax.get_ylim()[1] + ax.get_ylim()[0])/2.,
-                 "PRELIMINARY" ,horizontalalignment='center', verticalalignment='center',
+                 "PRELIMINARY", horizontalalignment='center', verticalalignment='center',
                  fontsize=54, alpha=0.3, color='grey', rotation=15)
 
 
@@ -112,22 +112,20 @@ class Index(object):
     def next(self, event):
         self.ind +=1
         start, stop = ax.get_xlim()
-        ax.set_xlim(int(start) + 100, int(stop) + 100)
+        ax.set_xlim(int(start) + self.step, int(stop) + self.step)
         self._updatePlot()
 
     def prev(self, event):
         self.ind -= 1
         start, stop = ax.get_xlim()
-        ax.set_xlim(int(start) - 100, int(stop) - 100)
+        ax.set_xlim(int(start) - self.step, int(stop) - self.step)
         self._updatePlot()
 
     def _updatePlot(self):
-        ax.autoscale(axis='y')
+        ax.autoscale(axis='y', tight=False)
         plt.draw()
 
 
-Index.start_point=200
-Index.stop_point=500
 callback = Index()
 axprev = plt.axes([0.7, 0.05, 0.1, 0.075]) #button coordinates and size
 axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
