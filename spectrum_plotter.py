cf0b9389681b250b19b30d4dddffb438b61db493
Ewa Adamska <ewa.adamska@fuw.edu.pl>
2019-11-04 15:52:37 +0100

    save button added

--- spectrum_plotter.py
+++ spectrum_plotter.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
 import pandas as pd
-
+from matplotlib.gridspec import GridSpec
 
 def read_data(filename, bg_filename=''):
     #prepare Data Frame
@@ -35,11 +35,12 @@ def addEnergy(energy, y, text, shiftx=0, fontsize=10):
 
 if __name__=='__main__':
 
-    GATES_LIST = (538, 444)
+    GATES_LIST = (181, 322)
     X_RANGE = (300, 500)
-    Y_RANGE = (0, 250)
+    Y_RANGE = (0, 100)
     SAVE_OUTPUT = False
     PRELIMINARY = False
+    MARK = True
 
     gatename="g{}g{}".format(GATES_LIST[0], GATES_LIST[1])
     try:
@@ -50,17 +51,19 @@ if __name__=='__main__':
     try:
         gamma_marks = pd.read_excel('gamma_marks_{}.xlsx'.format(gatename))
     except FileNotFoundError:
-        gamma_marks = False
-    # fig = plt.figure(figsize=(10, 4))
-    # ax = fig.add_subplot()
+        MARK = False
+
+    # fig, ax_array = plt.subplots(ncols=1, nrows=2)
+    # ax, ax2 = ax_array
+
 
     fig, ax = plt.subplots()
     fig.set_size_inches(10, 5)
-    fig.subplots_adjust(bottom=0.5)
+
     ax.tick_params(axis='both', labelsize=14)
     ax.set_xlabel('Energy (keV)', fontsize=12)
     ax.set_ylabel('Number of counts', fontsize=12)
-    plt.subplots_adjust(bottom=0.15)
+    plt.subplots_adjust(bottom=0.25)
 
     plt.ylim(Y_RANGE[0], Y_RANGE[1])
     plt.xlim(X_RANGE[0], X_RANGE[1])
@@ -69,10 +72,10 @@ if __name__=='__main__':
     box_width = 0.2 * (ax.get_xlim()[1] - ax.get_xlim()[0])
     box_height = 0.1 * (ax.get_ylim()[1] - ax.get_ylim()[0])
 
-    ax.text(ax.get_xlim()[1]-1.2*box_width, ax.get_ylim()[1]-1.1*box_height, "gate {} - {} keV ".format(GATES_LIST[0], GATES_LIST[1]), fontsize=12)
+    gt = ax.text(ax.get_xlim()[1]-1.2*box_width, ax.get_ylim()[1]-1.1*box_height, "gate {} - {} keV ".format(GATES_LIST[0], GATES_LIST[1]), fontsize=12)
 
     #adding marks:
-    if gamma_marks:
+    if MARK:
         for index, gamma in gamma_marks.iterrows():
             if gamma.type == 'text':
                 addEnergy(energy=int(gamma.energy), text="{} ({})".format(gamma.energy, gamma.isotope), y=gamma.y, shiftx=gamma.shiftx, fontsize=12)
@@ -97,14 +100,13 @@ if __name__=='__main__':
 
     if SAVE_OUTPUT:
         plt.savefig("Output/{}_{}-{}.svg".format(gatename, int(ax.get_xlim()[0]), int(ax.get_xlim()[1])))
-        plt.savefig("Output/{}_{}-{}.png".format(gatename, int(ax.get_xlim()[0]), int(ax.get_xlim()[1])))
+        # plt.savefig("Output/{}_{}-{}.png".format(gatename, int(ax.get_xlim()[0]), int(ax.get_xlim()[1])))
     # plt.show()
 
 
 
 
-
-
+# buttons
 class Index(object):
     ind = 0
     step = 100
@@ -123,16 +125,32 @@ class Index(object):
 
     def _updatePlot(self):
         ax.autoscale(axis='y', tight=False)
+        box_width = 0.2 * (ax.get_xlim()[1] - ax.get_xlim()[0])
+        box_height = 0.1 * (ax.get_ylim()[1] - ax.get_ylim()[0])
+
+        gt.set_position([ax.get_xlim()[1]-1.2*box_width, ax.get_ylim()[1]-1.1*box_height])
+
         plt.draw()
 
+class Utilities(object):
+    def save(self, event):
+        plt.savefig("Output/{}_{}-{}.svg".format(gatename, int(ax.get_xlim()[0]), int(ax.get_xlim()[1])))
 
+
+# prev & next
 callback = Index()
 axprev = plt.axes([0.7, 0.05, 0.1, 0.075]) #button coordinates and size
 axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
 bnext = Button(axnext, 'Next')
 bprev = Button(axprev, 'Previous')
 
+# save
+button = Utilities()
+axsave = plt.axes([0.91, 0.8, 0.075, 0.075]) #button coordinates and size
+bsave = Button(axsave, 'Save')
+
 bnext.on_clicked(callback.next)
 bprev.on_clicked(callback.prev)
+bsave.on_clicked(button.save)
 
 plt.show()
